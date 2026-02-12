f7959ef1bbb4bd74cae1b2447526dcf05b3c3927
Bharath R <107768150+MrWonder2@users.noreply.github.com>
2026-02-12 12:43:48 +0530

    final I hope

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
     // TODO: Write your program here to print the required output
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
-        System.out.println("Hello, Java");
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
 
 }
